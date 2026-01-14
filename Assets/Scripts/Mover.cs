68114ef54605efeb831675a0929fad7a9cf15401
Gumiho2504 <hemchanmetrey123@gmail.com>
2026-01-14 14:22:05 +0700

    add scene

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Mover script has started.");
+        transform.Translate(0, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
